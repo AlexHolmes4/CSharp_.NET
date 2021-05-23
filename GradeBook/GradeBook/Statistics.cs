6caf269b4de9fc6d6c7957fec643b40026ea099a
Alex Holmes <alexgholmes@hotmail.com>
2021-05-23 15:19:11 +0800

    added

---
+++ GradeBook/GradeBook/Statistics.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeBook
+{
+    public class Statistics
+    {
+        public double Average;
+        public double High;
+        public double Low;
+
+    }
+}

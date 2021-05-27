2962a577b69c6d385455632520557fb884f8a243
Alex Holmes <alexgholmes@hotmail.com>
2021-05-27 19:59:27 +0800

    added employees class in collection types

---
+++ CollectionTypes/CollectionTypes/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionTypes
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public int DepartmentID { get; set; }
+
+    }
+}
--- CollectionTypes/CollectionTypes/Program.cs
+++ CollectionTypes/CollectionTypes/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 
 /*
 Purpose of Project is for unit testing only, see CollectionTypesTests proj. 
@@ -6,11 +8,46 @@ Purpose of Project is for unit testing only, see CollectionTypesTests proj.
 
 namespace CollectionTypes
 {
+    public class EmployeeComparer : IEqualityComparer<Employee>
+    {
+        public bool Equals(Employee x, Employee y)
+        {
+            return String.Equals(x.Name, y.Name);
+        }
+
+        public int GetHashCode([DisallowNull] Employee obj)
+        {
+            return obj.Name.GetHashCode();
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var departments = new SortedDictionary<string, HashSet<Employee>>();
+
+            departments.Add("Sales", new HashSet<Employee>(new EmployeeComparer()));
+            departments["Sales"].Add(new Employee { Name = "Joy" });
+            departments["Sales"].Add(new Employee { Name = "Dani" });
+            departments["Sales"].Add(new Employee { Name = "Dani" });
+
+            departments.Add("Engineering", new HashSet<Employee>(new EmployeeComparer()));
+            departments["Engineering"].Add(new Employee { Name = "Scott" });
+            departments["Engineering"].Add(new Employee { Name = "Alex" });
+            departments["Engineering"].Add(new Employee { Name = "Dani" });
+
+            foreach (var department in departments)
+            {
+                Console.WriteLine(department.Key);
+                foreach (var employee in department.Value)
+                {
+                    Console.WriteLine(employee.Name);
+                }
+            }
+
+
+
         }
     }
 }

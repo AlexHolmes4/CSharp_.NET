56ad80132ff21f896220d4dd7c3df9a9ec243e5e
Alex Holmes <alexgholmes@hotmail.com>
2021-05-28 14:11:58 +0800

    generic methods added, and event

---
+++ DataStructures/DataStructures/BufferExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    public static class BufferExtensions
+    {
+
+        public static void Dump<T>(this IBuffer<T> buffer, PrinterDelegate print)
+        {
+            foreach (var item in buffer)
+            {
+                print(item);
+            }
+        }
+
+        public static IEnumerable<TOutput> AsEnumerableOf<T, TOutput>(this IBuffer<T> buffer)
+        {
+            var converter = TypeDescriptor.GetConverter(typeof(T));
+            foreach (var item in buffer)
+            {
+                TOutput result = (TOutput)converter.ConvertTo(item, typeof(TOutput));
+                yield return result;
+            }
+        }
+        
+
+    }
+}
--- DataStructures/DataStructures/Program.cs
+++ DataStructures/DataStructures/Program.cs
@@ -4,25 +4,30 @@ namespace DataStructures
 {
     class Program
     {
+
+
         static void Main(string[] args)
         {
-            var buffer = new Buffer<double>();
+            var buffer = new CircularBuffer<double>(capacity:3);
+            buffer.ItemDiscardedEventHandler += ItemDiscarded;
 
             ProcessInput(buffer);
+
+           // Converter<double, DateTime> converter = d => new DateTime(2010, 1, 1).AddDays(d);
+            var asDates = buffer.Map(d => new DateTime(2010, 1, 1).AddDays(d));
+            foreach (var date in asDates)
+            {
+                Console.WriteLine(date);
+            }
+
+            buffer.Dump(d => Console.WriteLine(d));
 
             ProcessBuffer(buffer);
         }
 
-        private static void ProcessBuffer(IBuffer<double> buffer)
+        private static void ItemDiscarded(object sender, ItemDiscardedEventArgs<double> e)
         {
-            var sum = 0.0;
-
-            Console.WriteLine("Buffer: ");
-            while (!buffer.IsEmpty)
-            {
-                sum += buffer.Read();
-            }
-            Console.WriteLine(sum);
+            Console.WriteLine();
         }
 
         private static void ProcessInput(IBuffer<double> buffer)
@@ -39,6 +44,18 @@ namespace DataStructures
                 }
                 break;
             }
+        }
+
+        private static void ProcessBuffer(IBuffer<double> buffer)
+        {
+            var sum = 0.0;
+
+            Console.WriteLine("Buffer: ");
+            while (!buffer.IsEmpty)
+            {
+                sum += buffer.Read();
+            }
+            Console.WriteLine(sum);
         }
     }
 }

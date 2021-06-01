5790464dca2cda6eed8ae4834e06d953ee4ae5ed
Alex Holmes <alexgholmes@hotmail.com>
2021-06-01 15:54:41 +0800

    collection of collection and LINQ

---
+++ Collections/PopulationsDemo/CollectionsOfCollections/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CollectionsOfCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\Users\alexg\Desktop\Code\GitHub\CSharp\Collections\PopulationsDemo\Pop by Largest Final.csv";
+            CsvReader reader = new CsvReader(filePath);
+
+            Dictionary<string, List<Country>> countries = reader.ReadAllCountries();
+            foreach (string region in countries.Keys)
+                Console.WriteLine(region);
+
+            Console.WriteLine("Which of the above regions do you want? ");
+            string chosenRegion = Console.ReadLine();
+
+            if (countries.ContainsKey(chosenRegion))
+            {
+                foreach (Country country in countries[chosenRegion].OrderBy(x => x.Name).Take(10))
+                    Console.WriteLine($"{PopulationFormatter.FormatPopulation(country.Population).PadLeft(15)}: {country.Name}");
+            }
+            else
+                Console.WriteLine("That is not a valid region");
+        }
+    }
+}
--- Collections/PopulationsDemo/PopulationsList/Program.cs
+++ Collections/PopulationsDemo/PopulationsList/Program.cs
@@ -12,13 +12,34 @@ namespace PopulationsList
 
             List<Country> countries = reader.ReadAllCountries();
 
-            Console.WriteLine("Hightest Population by Countries Data");
-            for (int i = 0; i < countries.Count; i++)
+            Console.WriteLine("Enter no. of countries to display> ");
+            bool inputIsInt = int.TryParse(Console.ReadLine(), out int userInput);
+            if (!inputIsInt || userInput <= 0)
             {
-                Country country = countries[i];
-                Console.WriteLine($"{PopulationFormatter.FormatPopulation(country.Population).PadLeft(15)}: {country.Name}");
+                Console.WriteLine("You must type a positive integer.\nExiting");
+                Environment.Exit(0);
             }
-            Console.WriteLine($"There were {countries.Count} countires in the data-set");
+            
+            Console.WriteLine("Hightest Population by Countries Data");
+            int maxToDisplay = userInput;
+            int i;
+            for (i = 0; i < countries.Count; i++)  // reverse (i = countries.Count - 1; i >= 0; i--)  
+            {
+                if (i > 0 && (i % maxToDisplay == 0))
+                {
+                    Console.WriteLine("Hit return to continue, anything else to quit>");
+                    if (Console.ReadLine() != "")
+                        break;
+                }
+                Country country = countries[i];
+                Console.WriteLine($"{i+1}: {PopulationFormatter.FormatPopulation(country.Population).PadLeft(15)}: {country.Name}");
+            }
+            if (i != countries.Count)
+            {
+                Console.WriteLine($"There were {countries.Count - i+1} countires remaining in the data-set");
+            }
+            else
+                Console.WriteLine("All countries retrieved.");
         }
     }
 }

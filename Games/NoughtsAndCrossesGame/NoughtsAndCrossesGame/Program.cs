e2f87a5660c31ff5901c6650154a594a2542df14
Alex Holmes <alexgholmes@hotmail.com>
2021-06-02 19:55:18 +0800

    dicegame added

---
+++ Games/NoughtsAndCrossesGame/NoughtsAndCrossesGame/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace NoughtsAndCrossesGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game game = new Game();
+            game.PlayGame();
+            Console.WriteLine("Game over");
+        }
+    }
+}
---
+++ Games/NoughtsAndCrossesGame/NoughtsAndCrossesGame/Square.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoughtsAndCrossesGame
+{
+    public enum Player { NoOne = 0, Noughts, Crosses}
+    public struct Square
+    {
+        public Player Owner { get;  }
+
+        public Square(Player owner)
+        {
+            this.Owner = owner;
+        }
+
+        public override string ToString()
+        {
+            switch (Owner)
+            {
+                case Player.NoOne:
+                    return ".";
+                case Player.Noughts:
+                    return "O";
+                case Player.Crosses:
+                    return "X";
+                default:
+                    throw new Exception("Invalid State");
+            }
+        }
+    }
+}

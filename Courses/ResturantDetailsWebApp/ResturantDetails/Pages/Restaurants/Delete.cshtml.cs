250b281d6fe2b61fddfffd32f38e0a2a996b939b
Alex Holmes <alexgholmes@hotmail.com>
2021-06-15 19:47:29 +0800

    delete page added

---
+++ Courses/ResturantDetailsWebApp/ResturantDetails/Pages/Restaurants/Delete.cshtml.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ResturantDetails.BL;
+using ResturantDetails.DL;
+
+namespace RestaurantsDetails.Pages.Restaurants
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly IRestaurantData restaurantData;
+
+        public Restaurant Restaurant { get; set; }
+
+        public DeleteModel(IRestaurantData restaurantData)
+        {
+            this.restaurantData = restaurantData;
+        }
+
+        public IActionResult OnGet(int restaurantId)
+        {
+            Restaurant = restaurantData.GetById(restaurantId);
+            return Restaurant == null ? RedirectToPage("./NotFound") : Page();
+        }
+
+        public IActionResult OnPost(int restaurantId)
+        {
+            var restaurant = restaurantData.Delete(restaurantId);
+            restaurantData.Commit();
+            if (restaurant == null)
+            {
+                RedirectToPage("./NotFound");
+            }
+
+            TempData["Message"] = $"{restaurant.Name} deleted";
+            return RedirectToPage("./List");
+        }
+    }
+}
